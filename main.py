b5558f2f2dc617846710fe47833323f26d964636
Denys Bokov <denisbokov1703@gmail.com>
2026-09-13 12:31:14 +0200

    feat: setup scraping from webscraper

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import dataclasses
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://webscraper.io/"
+HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/allinone")
+
+
+@dataclasses.dataclass
+class Product:
+    title: str
+    description: str
+    price: float
+    rating: int
+    num_of_reviews: int
+
+
+def parse_single_product(product: Tag) -> Product:
+    product_obj = {
+        "title": product.select_one('.title')["title"],
+        "description": product.select_one('.description').text
+    }
+
+    print('product', product_obj)
+
+def get_home_products():
+    text = requests.get(HOME_URL).content
+    soup = BeautifulSoup(text, 'html.parser')
+
+    products = soup.select(".card-body")
+
+    return [parse_single_product(product) for product in products]
+
 def main():
-    pass
+    get_home_products()
 
 if __name__ == '__main__':
     main()
